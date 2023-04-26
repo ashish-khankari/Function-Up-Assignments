393badf4445bc55b84e6ada875e674fa093c231b
ashish@123456 <ashishkhankari0927@gmail.com>
2023-04-26 22:41:25 +0530

    This is project for Counter-app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+<script src="https://kit.fontawesome.com/3bb78fc623.js" crossorigin="anonymous"></script>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

a77fbdeb10f91784ef68d061b46e6d809798ea83
ashish@123456 <ashishkhankari0927@gmail.com>
2023-05-02 01:50:34 +0530

    Changing Images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css'
-import Name from './Components/Number';
+import ImageChange from './Components/ImageSlider';
 export default function App() {
   return (
     <div className='App'>
-      <Name />
+      <ImageChange />
     </div>
   );
 }
+
+
+

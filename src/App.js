e97eb84f691edb5f2f8a1ac607f30011d52e8b8b
ashish@123456 <ashishkhankari0927@gmail.com>
2023-05-09 02:46:38 +0530

    Media Query Code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css'
-import DogApi from './Components/Dog-Axios';
+import Header from './Components/Organism/Header';
 export default function App() {
   return (
     <div className='App'>
-      <DogApi/>
+     <Header/>
     </div>
   );
 }
